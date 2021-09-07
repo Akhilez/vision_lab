458168c26a37e72c5f5d592e95eb6b9b5ad023e6
Akhilez <lightrescuer@gmail.com>
2021-09-06 21:28:49 -0500

    working on new synthetic data generator.

--- companies/tychon/dataset_creator.py
+++ companies/tychon/dataset_creator.py
@@ -1,3 +1,14 @@
+from random import randint, random
+from typing import Optional, Any, Tuple
+
+import cv2
+import numpy as np
+import requests
+from matplotlib import pyplot as plt
+from pydantic import BaseModel
+from PIL import Image
+
+
 # 160 224
 # 320 448
 # 480 672
@@ -6,22 +17,53 @@
 # https://loremflickr.com/1280/720/painting
 
 """
-1. Divide the image into 1-3 columns
-2. For each column
-    1. select a perspective ratio
-    2. Create random trapezoids into the column
-    3. Random painting into the trapezoid for each trapezoid
-3. Optional: Place random objects into the frame.
+- Divide the image into 1-3 columns
+- For each column:
+    - select number of frames / grid size.
+    - for each frame:
+        - Find (h, w) in from (cell_h * 0.5) to (cell_h * 0.9).
+        - place a random painting.
+    - Select if angled or flat.
+    - if angled, randomly select angle.
 """
-from random import randint
-from typing import Optional, Any
 
-import cv2
-import numpy as np
-import requests
-from matplotlib import pyplot as plt
-from pydantic import BaseModel
-from PIL import Image
+
+def generate_dataset(count=10, height = 448, width = 448):
+    for image_i in range(count):
+        num_columns = randint(1, 3)
+        for column_i in range(num_columns):
+            n_rows = randint(1, 3)
+            n_cols = randint(1, 2)
+
+            cell_h = height // n_rows
+            cell_w = width // n_cols
+
+            for row_i in range(n_rows):
+                for col_i in range(n_cols):
+                    # If include image or not.
+                    if random() > 0.5:
+                        continue
+
+                    x1, y1, x2, y2 = get_random_frame_in_cell(cell_h, cell_w, min_h=0.5, min_w=0.5, padding=0.1)
+                    h, w = x2 - x1, y2 - y1
+
+
+def get_random_frame_in_cell(cell_h, cell_w, min_h, min_w, padding) -> Tuple[int, int, int, int]:
+    # y1 -> 0 to ((h - min_h) / 2)
+
+    min_h = int(cell_h * min_h)
+    min_w = int(cell_w * min_w)
+    padding = (cell_h + cell_w) * padding // 2
+
+    y_bound = ((cell_h - min_h) // 2)
+    x_bound = ((cell_w - min_w) // 2)
+
+    x1 = randint(padding, x_bound)
+    x2 = randint(cell_w - x_bound, cell_w - padding)
+    y1 = randint(padding, y_bound)
+    y2 = randint(cell_h - y_bound, cell_h - padding)
+
+    return x1, y1, x2, y2
 
 
 class Coordinate(BaseModel):
